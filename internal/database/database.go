a4015d5cbdf6f3da5cc1df3be2cee68d0095cc40
Vladimir Alekseev <vcs@avs.guru>
2024-06-10 14:28:14 +0300

    added docker app

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,16 +1,24 @@
 package database
 
 import (
-	"database/sql"
-	_ "github.com/lib/pq"
-	"log"
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+    "log"
+    "os"
 )
 
 func Connect() *sql.DB {
-	connStr := "user=user password=password dbname=dating_db sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+
+    connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable", dbUser, dbPassword, dbName, dbHost, dbPort)
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return db
 }

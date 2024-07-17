afd122caaa77e2ff82b193820c2441edbea4d55c
Vladimir Alekseev <vcs@avs.guru>
2024-07-17 16:42:40 +0300

    rename

--- docker/db/master/init-master.sh
+++ docker/db/master/init-master.sh
@@ -5,6 +5,9 @@ echo "Starting init-master.sh with user $POSTGRES_USER and database $POSTGRES_DB
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
+cp /docker-entrypoint-initdb.d/postgresql.conf ${PGDATA}/postgresql.conf
+cp /docker-entrypoint-initdb.d/pg_hba.conf ${PGDATA}/pg_hba.conf
+
 # Старт PostgreSQL
 if pg_ctl -D "$PGDATA" status; then
     echo "PostgreSQL is already running"
@@ -19,10 +22,26 @@ sleep 5
 
 # Создание пользователей и баз данных
 psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'password';
+    CREATE USER replicator REPLICATION LOGIN ENCRYPTED PASSWORD 'password';
+
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        birth_date DATE,
+        gender CHAR(1),
+        interests TEXT,
+        city VARCHAR(50),
+        username VARCHAR(50),
+        password VARCHAR(100)
+    );
 EOSQL
+
+#cp /docker-entrypoint-initdb.d/pg_hba.conf "/var/lib/postgresql/pg_hba.conf"
 
 # Перезапуск сервера для применения конфигурации
 pg_ctl -D "$PGDATA" restart
+
+ls -la /var/lib/postgresql/
 
 echo "init-master.sh completed"
--- docker/db/slave/init-slave.sh
+++ docker/db/slave/init-slave.sh
@@ -3,9 +3,11 @@ set -e
 
 echo "Starting init-slave.sh"
 
+#cp /docker-entrypoint-initdb.d/pg_hba.conf "$PGDATA/pg_hba.conf"
+
 until psql -h "db-master" -U "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Master is unavailable - sleeping"
-  sleep 1
+  sleep 5
 done
 
 PGPASSWORD=password pg_basebackup -h db-master -D ${PGDATA} -U replicator -P -R
@@ -13,7 +15,7 @@ PGPASSWORD=password pg_basebackup -h db-master -D ${PGDATA} -U replicator -P -R
 #echo "primary_conninfo = 'host=db-master port=5432 user=${DB_USER}_replicator password=${DB_PASSWORD}'" >> ${PGDATA}/postgresql.conf
 
 cp /docker-entrypoint-initdb.d/postgresql.conf ${PGDATA}/postgresql.conf
-cp /docker-entrypoint-initdb.d/pg_hba.conf ${PGDATA}/pg_hba.conf
+#cp /docker-entrypoint-initdb.d/pg_hba.conf ${PGDATA}/pg_hba.conf
 
 pg_ctl -D "${PGDATA}" -o "-c listen_addresses='*'" -w start
 
